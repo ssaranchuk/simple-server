425428025a9a3fff2e6534ba7c9ee0d64dde0269
Serhii Saranchuk <serhii_saranchuk@l121tiffanyr.abcfinancial.net>
2024-01-19 15:37:32 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next()
-})
+app.use(cors())
 
 app.get('/test-endpoint', (req, res) => {
   res.send({data: 'test response'})
 })
+
+app.get('/test-endpoint2', (req, res) => {
+  res.send({data: 'test response'})
+})
+
 
 app.listen(port, () => {
   console.log(`Demo app is up and listening to port ${port}`);
